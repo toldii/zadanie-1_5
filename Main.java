523b1a5e1c90c3a1cee166da19d1131588dc7259
toldii1 <100698275+toldii@users.noreply.github.com>
2022-03-27 15:39:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args) {
+   Scanner scan = new Scanner(System.in);
+    System.out.println("Liczby nieparzyste z przedziału 50-100 to:");
+    int a;
+   
+    for(int i=51; i<=100;  i+=2) {
+     
+      System.out.println("i=" + i);
+}
+}
+}
